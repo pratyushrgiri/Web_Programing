18538f4e02b4d51168df007885317352b643ce1d
Pratyush Raj Giri <pratyushrajgireee@gmail.com>
2026-01-15 12:19:21 +0545

    Learning javascript

---
+++ javascript/firstprogram.js
@@ -0,0 +1,16 @@
+// this is how to print to the console in JavaScript
+console.log("Hello, World!");
+//how to declare a variable
+age=25;
+    // lets print this age
+    console.log(age);
+
+    /*To declare all types of variables*/
+        age=30; //integer variables
+        price=99.99;//float variables
+        fullName="Pratyush Raj Giri"; //String variables
+    // lets print these on the console then
+        console.log(age);
+        console.log(price);
+        console.log(fullName);
+
